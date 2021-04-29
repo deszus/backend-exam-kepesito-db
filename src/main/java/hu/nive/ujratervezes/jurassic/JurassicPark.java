a38447d3343b4692b5ed1ca4bb57c499a1f51717
deszus <68805470+deszus@users.noreply.github.com>
2021-04-29 11:53:23 +0200

    1st upload

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,20 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> result = new ArrayList<>();
+        try (Connection DBcon = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT * FROM dinosaur WHERE expected < actual ORDER BY breed";
+            PreparedStatement st = DBcon.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                result.add(rs.getString("breed"));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return result;
+    }
 }
